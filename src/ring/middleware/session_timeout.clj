2f5a7baa4a62cf78d249509a723636e3d1c12999
James Reeves <jreeves@weavejester.com>
2014-05-21 19:00:17 +0100

    Initial commit

---
+++ src/ring/middleware/session_timeout.clj
@@ -0,0 +1,1 @@
+(ns ring.middleware.session-timeout)
---
+++ test/ring/middleware/session_timeout_test.clj
@@ -0,0 +1,7 @@
+(ns ring.middleware.session-timeout-test
+  (:use clojure.test
+        ring.middleware.session-timeout))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
