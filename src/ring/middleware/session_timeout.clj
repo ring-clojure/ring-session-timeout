8adeb09e8d5d8890041979e42c17d68651a596b0
James Reeves <jreeves@weavejester.com>
2014-05-24 23:08:52 +0100

    Add absolute timeout middleware

--- src/ring/middleware/session_timeout.clj
+++ src/ring/middleware/session_timeout.clj
@@ -1,27 +1,55 @@
-(ns ring.middleware.session-timeout)
+(ns ring.middleware.session-timeout
+  "Middleware for managing session timeouts.")
 
 (defn- current-time []
   (quot (System/currentTimeMillis) 1000))
 
 (defn wrap-idle-session-timeout
   "Middleware that times out idle sessions after a specified number of seconds.
+
   If a session is timed out, the timeout-response option is returned. This is
   usually a redirect to the login page.
 
   The following options are accepted:
 
-  :timeout          - the idle timeout in seconds
+  :timeout          - the idle timeout in seconds (default 600 seconds)
   :timeout-response - the response to send if an idle timeout occurs"
   [handler {:keys [timeout timeout-response] :or {timeout 600}}]
   {:pre [(integer? timeout) (map? timeout-response)]}
   (fn [request]
     (let [session  (:session request {})
           end-time (::idle-timeout session)]
-      (prn session)
+      (if (and end-time (< end-time (current-time)))
+        (assoc timeout-response :session nil)
+        (let [response (handler request)
+              end-time (+ (current-time) timeout)
+              session  (-> (:session response session)
+                           (assoc ::idle-timeout end-time))]
+          (assoc response :session session))))))
+
+(defn wrap-absolute-session-timeout
+  "Middleware that times out sessions after a specified number of seconds,
+  regardless of whether the session is being used or idle. This places an upper
+  limit on how long a compromised session can be exploited.
+
+  If a session is timed out, the timeout-response option is returned. This is
+  usually a redirect to the login page.
+
+  The following options are accepted:
+
+  :timeout          - the absolute timeout in seconds
+  :timeout-response - the response to send if an idle timeout occurs"
+  [handler {:keys [timeout timeout-response]}]
+  {:pre [(integer? timeout) (map? timeout-response)]}
+  (fn [request]
+    (let [session  (:session request {})
+          end-time (::absolute-timeout session)]
       (if (and end-time (< end-time (current-time)))
         (assoc timeout-response :session nil)
         (let [response (handler request)
               session  (:session response session)]
-          (assoc-in response
-                    [:session ::idle-timeout]
-                    (+ (current-time) timeout)))))))
+          (if (or (nil? session) (and end-time (not (contains? response :session))))
+            response
+            (let [end-time (or end-time (+ (current-time) timeout))
+                  session  (assoc session ::absolute-timeout end-time)]
+              (assoc response :session session))))))))
--- test/ring/middleware/session_timeout_test.clj
+++ test/ring/middleware/session_timeout_test.clj
@@ -9,16 +9,16 @@
    :headers {"Content-Type" "text/plain"}
    :body "ok"})
 
-(def idle-response
+(def timeout-response
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "idle timeout"})
+   :body "timeout"})
 
 (def idle-handler
   (-> (constantly ok-response)
       (wrap-idle-session-timeout
        {:timeout 600
-        :timeout-response idle-response})))
+        :timeout-response timeout-response})))
 
 (defmacro with-time [time & body]
   `(with-redefs [timeout/current-time (constantly ~time)]
@@ -41,5 +41,31 @@
     (let [request  (-> (mock/request :get "/")
                        (assoc :session {::timeout/idle-timeout 1400000600}))
           response (with-time 1400000700 (idle-handler request))]
-      (is (= (:body response) "idle timeout"))
+      (is (= (:body response) "timeout"))
+      (is (= (:session response :empty) nil)))))
+
+(def absolute-handler
+  (-> (constantly ok-response)
+      (wrap-absolute-session-timeout
+       {:timeout 600
+        :timeout-response timeout-response})))
+
+(deftest test-idle-timeout
+  (testing "timeout added to session"
+    (let [response (with-time 1400000000 (absolute-handler (mock/request :get "/")))]
+      (is (= (:body response) "ok"))
+      (is (= (:session response) {::timeout/absolute-timeout 1400000600}))))
+
+  (testing "not timed out"
+    (let [request  (-> (mock/request :get "/")
+                       (assoc :session {::timeout/absolute-timeout 1400000600}))
+          response (with-time 1400000500 (absolute-handler request))]
+      (is (= (:body response) "ok"))
+      (is (not (contains? response :session)))))
+
+  (testing "timed out"
+    (let [request  (-> (mock/request :get "/")
+                       (assoc :session {::timeout/absolute-timeout 1400000600}))
+          response (with-time 1400000700 (absolute-handler request))]
+      (is (= (:body response) "timeout"))
       (is (= (:session response :empty) nil)))))
