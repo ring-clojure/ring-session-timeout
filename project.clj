ab581db4c1a062015f8ad1f53ade38df039b267b
James Reeves <jreeves@weavejester.com>
2023-04-09 02:42:47 +0100

    Update minimum Clojure version to 1.7.0
    Add test profiles for Clojure versions up to 1.11.1.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/ring-clojure/ring-session-timeout"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :plugins [[lein-codox "0.10.1"]]
   :codox
   {:project {:name "Ring-Session-Timeout"}
@@ -11,9 +11,10 @@
    :output-path "codox"
    :source-uri
    "https://github.com/ring-clojure/ring-session-timeout/blob/{version}/{filepath}#L{line}"}
-  :aliases {"test-all" ["with-profile" "default:+1.6:+1.7:+1.8" "test"]}
+  :aliases {"test-all" ["with-profile" "default:+1.8:+1.9:+1.10:+1.11" "test"]}
   :profiles
   {:dev {:dependencies [[ring-mock "0.1.5"]]}
-   :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+   :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10 {:dependencies [[org.clojure/clojure "1.10.3"]]}
+   :1.11 {:dependencies [[org.clojure/clojure "1.11.1"]]}})
